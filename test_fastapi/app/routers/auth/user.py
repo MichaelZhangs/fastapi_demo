9052aac2aca06c7829b02a32c471e5d3b8fd98f5
MichaelZhangs <58901344+MichaelZhangs@users.noreply.github.com>
2022-06-02 11:20:59 +0800

    Add files via upload

--- test_fastapi/app/routers/auth/user.py
+++ test_fastapi/app/routers/auth/user.py
@@ -1,12 +1,13 @@
 from config.configs import api
 from fastapi import APIRouter, Request
-import datetime
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas import schemas
 from app.config.database_config import async_session, SessionLocal, engine
 from app.crud import crud
 from typing import List
+from app.routers.auth.auth_token import UserToken
+from app.utils.factory import Response
 
 
 router = APIRouter(prefix="/user")
@@ -31,8 +32,9 @@ async def request_info(request: Request):
 
 def f():
     print("Hello FastApi .....")
-
-
+import datetime
+cur_time = datetime.datetime.now()
+next_time = cur_time + datetime.timedelta(seconds=15)
 # @router.get("/")
 # def read_root():
 #     api.logger.info("进入 user 路由")
@@ -48,7 +50,6 @@ def get_db():
     finally:
         db.close()
 
-
 # async def get_db():
 #     async with async_session() as session:
 #         db = session
@@ -60,9 +61,7 @@ def get_db():
 
 @router.post("/create/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    print("user_dict = ", schemas.UserCreate.password)
     db_user = crud.get_user_by_email(db, email=user.email)
-    print(db_user)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
@@ -77,17 +76,17 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @router.get("/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
-    print("response_model = ",schemas.User.schema_json())
-    print(db_user.to_dict())
+    print("response_model = ",schemas.User)
+    # print(db_user.to_dict())
+    api.logger.info(db_user.to_dict())
+    api.scheduler.add_job(f, "cron", second=5, timezone="Asia/Shanghai")
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
 @router.post("/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
+def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
@@ -95,3 +94,19 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@router.post("/login")
+def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    try:
+        password = user.password
+        email = user.email
+        d = crud.login_required(db, email=email, password=password)
+        if d:
+            token = UserToken.get_token(user.dict())
+            print("token = ", token)
+            return Response.success(dict(token=token, user=user.email), msg="登录成功")
+        else:
+            return Response.failed(data= user.dict(), msg="密码不正确")
+    except Exception as e:
+        return Response.failed(e)
