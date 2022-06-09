2e66c9cc7f6b89e4157919cc026a1cf8c0e7eb27
MichaelZhangs <58901344+MichaelZhangs@users.noreply.github.com>
2022-06-09 08:56:22 +0800

    Add files via upload

--- test_fastapi/main.py
+++ test_fastapi/main.py
@@ -1,4 +1,7 @@
 import asyncio
+import datetime
+import time
+
 from config.configs import Config
 from log.logger import Log
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
@@ -7,6 +10,9 @@ from fastapi import Request, Response
 from app.routers import api
 from fastapi.middleware.cors import CORSMiddleware
 from app.config.database_config import async_session
+from utils.redis_help import redis_conn
+from log.new_log import create_logger, stdout_handler, daily_rotaiting_handler
+
 
 
 api.add_middleware(
@@ -39,8 +45,46 @@ def init_scheduler():
         'default': SQLAlchemyJobStore(url=Config.SQLALCHEMY_DATABASE_URI, engine_options={"pool_recycle": 1500},
                                       pickle_protocol=3)
     }
-    api.scheduler = AsyncIOScheduler(timezone="Asia/Shanghai")
+    api.scheduler = AsyncIOScheduler()
     api.scheduler.configure(jobstores=job_store)
     api.scheduler.start()
     api.logger = Log()
     api.logger.info("ApScheduler started success.        ✔")
+
+    api.new_log = create_logger("", (daily_rotaiting_handler(filename="./logs/new_log.log"), stdout_handler()))
+    api.new_log.info("新的日志打印来了")
+
+
+def f():
+    print("hello FastApi .......")
+    redis_conn.set("f_func", "1")
+    api.logger.error("hello FastApi .......")
+    api.logger.info("嘿嘿嘿额嘿嘿")
+
+def g():
+    print("hahahahha.......")
+    redis_conn.set("g_func", "1")
+    api.logger.warning("hahahahha.......")
+    api.logger.debug("哈哈哈哈哈哈哈哈")
+
+@api.on_event('startup')
+def aps_scheduler():
+    api.logger.debug(f"开始初始化定时任务......")
+    # f_v = redis_conn.get("f_func")
+    # g_v = redis_conn.get("g_func")
+    # 启动前检查是否有定时任务
+    jobs = api.scheduler.get_jobs()
+    cur_time = str(datetime.datetime.now())
+    if jobs:
+        for job in jobs:
+            t = str(job.next_run_time)
+            if "." in t:
+                t0 = t.split(".")[0]
+                date_t0 = datetime.datetime.strptime(t0, "%Y-%m-%d %H:%M:%S")
+                if cur_time > str(date_t0 + datetime.timedelta(minutes=5)):
+                    api.scheduler.resume_job(job.id)
+                print(job.id, job.name)
+
+    else:
+        api.scheduler.add_job(f, "interval", minutes=5,  id=str(id(f)))
+        api.scheduler.add_job(g, "interval", minutes=3, id=str(id(g)))
